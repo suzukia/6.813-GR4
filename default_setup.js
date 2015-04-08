9f8af3af7edbba3c25d47e6c0c8043f7d4bc3b97
Ami Suzuki <asuzuki@mit.edu>
2015-04-07 22:43:58 -0400

    changed footer header importing

--- default_setup.js
+++ default_setup.js
@@ -10,13 +10,25 @@ function readFile(filePath, callback) {
 	xmlhttp.send();
 }
 
-function appendToBody(htmlFile) {
-	readFile(htmlFile, function(html) { 
-		$('body').append(html) 
+function appendToDiv(htmlFile, divId) {
+	readFile(htmlFile, function(html) {
+		$('#'+divId).append(html)
 	});
 }
 
+var appendStringToDiv = function (stringhtml, divId) {
+	$('#'+divId).append(stringhtml);
+}
+
+var footerString = '<div id="navbar-footer"><div id="chatbox"></div><div id="chattabs"></div></div>';
+
+var headerString = '<div id="navbar-header"><div id="navbar-title">[Navbar Title]</div><div id="navbar-buttons"></div></div>';
+
+
+
 $(document).ready(function() {
-	appendToBody('templates/header.html');
-	appendToBody('templates/footerchat.html');
+	// appendToDiv('templates/header.html', 'header');
+	// appendToDiv('templates/footerchat.html', 'footer');
+	appendStringToDiv(headerString, 'header');
+	appendStringToDiv(footerString, 'footer');
 });
