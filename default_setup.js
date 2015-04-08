636ad8f27670c7ab7a9bc4eae3029014285a812a
Ami Suzuki <asuzuki@mit.edu>
2015-04-07 23:13:18 -0400

    changing nav bar title

--- default_setup.js
+++ default_setup.js
@@ -1,28 +1,28 @@
-function readFile(filePath, callback) {
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function(){
-	  if(xmlhttp.status == 200 && xmlhttp.readyState == 4){
-	    var txt = xmlhttp.responseText;
-	    callback(txt);
-	  }
-	};
-	xmlhttp.open("GET",filePath,true);
-	xmlhttp.send();
-}
+// function readFile(filePath, callback) {
+// 	var xmlhttp = new XMLHttpRequest();
+// 	xmlhttp.onreadystatechange = function(){
+// 	  if(xmlhttp.status == 200 && xmlhttp.readyState == 4){
+// 	    var txt = xmlhttp.responseText;
+// 	    callback(txt);
+// 	  }
+// 	};
+// 	xmlhttp.open("GET",filePath,true);
+// 	xmlhttp.send();
+// }
 
-function appendToDiv(htmlFile, divId) {
-	readFile(htmlFile, function(html) {
-		$('#'+divId).append(html)
-	});
-}
+// function appendToDiv(htmlFile, divId) {
+// 	readFile(htmlFile, function(html) {
+// 		$('#'+divId).append(html)
+// 	});
+// }
 
 var appendStringToDiv = function (stringhtml, divId) {
 	$('#'+divId).append(stringhtml);
 }
 
-var footerString = '<div id="navbar-footer"><div id="chatbox"></div><div id="chattabs"></div></div>';
-
 var headerString = '<div id="navbar-header"><div id="navbar-title">[Navbar Title]</div><div id="navbar-buttons"></div></div>';
+
+var footerString = '<div id="navbar-footer"><div id="chatbox"></div><div id="chattabs"></div></div>';
 
 
 
---
+++ javascript/home.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+ $('#navbar-title').text("This is Home");
+});
