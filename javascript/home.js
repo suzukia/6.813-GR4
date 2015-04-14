768ce98c583c7e764b6e78a4bbcba55e21e1d187
Ami Suzuki <asuzuki@mit.edu>
2015-04-13 21:05:19 -0400

    header fix

--- javascript/home.js
+++ javascript/home.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
  //$('#navbar-title').text("Welcome, Erik");
  $('#navbar-title').append($( "h2" ))
- $('#navbar-buttons').append($( "#friendRequests" ));
+ // $('#navbar-title').text("Welcome, Erik");
  $('#navbar-buttons').append($( "#gameInvitations" ));
+
+ $('#navbar-buttons').append($( "#friendRequests" ));
 });
 
