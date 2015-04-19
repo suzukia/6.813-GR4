529a58b40cb6ce8b31255fe17947ad290ec21306
Amadu Durham <amadu@mit.edu>
2015-04-19 18:27:18 -0400

    Almost completed home.html

---
+++ javascript/home.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$('#chat-bar').css('height',$(window).height());
+	$('#friends').css('max-height',$(window).height());
+	$('#friends').css('overflow-y', 'scroll');
+});
