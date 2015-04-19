fd9bbbd2e4ea3d92425cf3ac62ded6e6e6b65b56
Flora Tan <floratan@mit.edu>
2015-04-19 18:52:04 -0400

    fixing up alignment of chatbar on home.html

--- javascript/home.js
+++ javascript/home.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	$('#chat-bar').css('height',$(window).height());
-	$('#friends').css('max-height',$(window).height());
+	$('#friends').css('max-height',$(window).height()-120);
 	$('#friends').css('overflow-y', 'scroll');
 });
