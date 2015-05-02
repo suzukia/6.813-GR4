c216df73101edbc1466aa21c17aa65c33ddb2c4e
Flora Tan <floratan@mit.edu>
2015-05-02 19:49:47 -0400

    loading map image to fill entire body

--- javascript/map.js
+++ javascript/map.js
@@ -7,28 +7,38 @@ var challengesToQuestions = {};
 var chatName = "User1,User2,User3,User4";
 
 $(document).ready(function() {
+  // setup navigation bar and dictionaries
+  kltNavbar = $('.klt-navbar');
+  configNavbar();
+  setUpDictionaries();
+  
+  // group chat setup
   chatbox = openChat(chatName, gameMsgSentFunc);
   simulateInitGameConversation(chatbox, chatName);
 
-	map = localStorage.getItem("map");
-	if (map == null) {
-		currentMap = "Space"; // default
-	} else {
-		currentMap = map.name;
-	}
-    kltNavbar = $('.klt-navbar');
-    
-    currentAct = 0;				// default
-    configNavbar();
-    setUpDictionaries();
+  // loading data from local storage
+  map = localStorage.getItem("map");
+  if (map == null) {
+  	currentMap = "Space"; // default
+  } else {
+  	currentMap = map.name;
+  }
+  currentAct = 0;       // default
+  populateData();
+  
+  // document.getElementById('instructionContent').innerHTML = "Welcome to the " + currentMap + " map! Click anywhere on the map to start this challenge.";
+  // $('#instructionModal').modal('show');
 
-    var sp = new SketchPad("canvas-test");
-    sp.init(0);
+  console.log('../images/'+scenesToImages[currentMap][currentAct]);
+  var imageFile = scenesToImages[currentMap][currentAct];
+  document.getElementById("map-image").innerHTML="<img src='../images/map/"+currentMap.toLowerCase()+"/"+imageFile+"' alt='' height='800' width='1336'>";
+  $(".content").css("margin-top",0);
+  var sp = new SketchPad("canvas-test");
+  sp.init(0);
 
-    populateData();
+  console.log(challengesToQuestions.act1[0].title);
+});
 
-    console.log(challengesToQuestions.act1[0].title);
-  });
 
 function populateData() {
 
@@ -37,10 +47,10 @@ function populateData() {
 function setUpDictionaries() {
 	// We can use currentAct to index into the list of values and get the proper item
     // key (currentMap) -> value (name of scene or image)
-    mapsToScenes["Space"] 		= ["act1", "act2", "act3"];
-	mapsToScenes["Medieval"] 	= ["voyage1", "voyage2", "voyage3"];
-    scenesToImages["Space"] 	= ["act1.jpg", "act2.png", "act3.png"];
-	scenesToImages["Medieval"]  = ["voyage1.jpg", "voyage2.jpeg", "voyage3.jpg"];
+    mapsToScenes["Medieval"] 		= ["act1", "act2", "act3"];
+	mapsToScenes["Space"] 	= ["voyage1", "voyage2", "voyage3"];
+    scenesToImages["Medieval"] 	= ["act1.jpg", "act2.png", "act3.png"];
+	scenesToImages["Space"]  = ["voyage1.jpg", "voyage2.jpeg", "voyage3.jpg"];
 
 	// key (currentMap, currentAct) -> value (list of the names of questions)
 	challengesToQuestions["act1"]	 = [compositionOfMatter, lifeOfPi, famousComposers];
@@ -67,7 +77,7 @@ function Question(title, description, choices, correctAnswer) {
 $('#submit').click(function(){
 });
 
-$('#questionTable input').on('change', function() {
+$('#questionContainer input').on('change', function() {
    alert($('input[name=radioName]:checked', '#questionTable').val()); 
 });
 
