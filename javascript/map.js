209eda343ca3b66855daab9130d01be4bee8c9f1
Ami Suzuki <asuzuki@mit.edu>
2015-05-02 23:24:15 -0400

    reformat map.js, update page function

--- javascript/map.js
+++ javascript/map.js
@@ -7,7 +7,12 @@ var scenesToImages = {};
 var challengesToQuestions = {};
 // var firstMsgs = ["hey guys!","wassup", "heyy", "everyone ready, right?!"];
 
+
 $(document).ready(function() {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////// get current game data /////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // if not logged in, redirect to login
     var username = localStorage.getItem("username");
@@ -21,7 +26,6 @@ $(document).ready(function() {
         redirectTo("home.html");
     }
 
-
     // gameInfo format (passed on from previous page):
     // {"map":{"name":"Medieval","description":"Journey to your throne","icon":"../images/map/space/icon.gif"},
     //  "players":[{"_id":15,"_name":"Cinderella","_avatar":"../images/chat/avatar1.gif","_friends":[],"_friendReqs":{}}],
@@ -30,18 +34,12 @@ $(document).ready(function() {
     var players = formatUsers(gameInfo.players);
     var currentMap = gameInfo.map;
 
-    // set up chat name
-    var chatName = username;
-    for (var i=0; i< players.length; i++) {
-        chatName += ", " + players[i].name();
-    }
-    var gameInfo = getStorageItem("gameInfo");
-    var players = formatUsers(gameInfo.players);
-    console.log(players);
-    var map = gameInfo.map;
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////// set up map page ///////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // set up chat name
-    var chatName = localStorage.getItem("username");
+    // set up chat names
+    var chatName = username;
     for (var i=0; i< players.length; i++) {
         chatName += ", " + players[i].name();
     }
@@ -50,70 +48,49 @@ $(document).ready(function() {
     currentSceneIndex = 0;
     currentQuestionIndex = 0;
 
+    // setup navigation bar and dictionaries
+    kltNavbar = $('.klt-navbar');
+    configNavbar();
 
-  // setup navigation bar and dictionaries
-  kltNavbar = $('.klt-navbar');
-  configNavbar();
-  // setUpDictionaries();
+    // group chat setup
+    chatbox = openChat(chatName, gameMsgSentFunc);
+    //simulateInitGameConversation(chatbox, chatName);
+    //openChat(chatName, gameMsgSentFunc);
 
-  // group chat setup
-  chatbox = openChat(chatName, gameMsgSentFunc);
-  //simulateInitGameConversation(chatbox, chatName);
-  //openChat(chatName, gameMsgSentFunc);
+    // pop up instructions
+    document.getElementById('instructionContent').innerHTML = "Welcome to the " + currentMap.name + " map! Click anywhere on the map to start this challenge.";
+    $('#instructionModal').modal('show');
 
-  // loading data from local storage
-// <<<<<<< HEAD
-  // map = localStorage.getItem("map");
-  // if (map == null) {
-  // 	currentMap = "Space"; // default
-  // } else {
-  // 	currentMap = map.name;
-  // }
-  // currentAct = 2;       // default
-// =======
-//   map = localStorage.getItem("map");
-//   if (map == null) {
-//   	currentMap = "Space"; // default
-//   } else {
-//   	currentMap = map.name;
-//   }
+    // to make image fill the entire body of the website
+    $(".content").css("margin-top",0);
+    $(".content").css("padding-left",0);
+    $(".content").css("padding-right",0);
+    $(".klt-navbar").css("margin-bottom",0);
 
-  $('#feedback').hide();
-  $('#checkA').hide();
-  $('#checkB').hide();
-  $('#checkC').hide();
-  $('#checkD').hide();
-  // currentAct = 0;       // default
-  // currentChallenge = mapsToScenes[currentMap][currentAct];
-  // currentQuestion = challengesToQuestions[currentChallenge][currentAct];
-  currentQuestion = currentMap.scenes[currentSceneIndex].questions[currentQuestionIndex];
-// >>>>>>> 0d27d55ebc7aca05a80994de4bd35d2023a8cd5b
-  populateData();
+    updatePage();
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////// CHALLENGE MODAL //////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-  document.getElementById('instructionContent').innerHTML = "Welcome to the " + currentMap.name + " map! Click anywhere on the map to start this challenge.";
-  $('#instructionModal').modal('show');
-
-  // console.log('../images/'+scenesToImages[currentMap][currentAct]);
-  // var imageFile = scenesToImages[currentMap][currentAct];
-  document.getElementById("map-image").innerHTML="<img src='"+currentMap.scenes[currentSceneIndex].image+"' alt='' height='800' width='1400'>";
-
-  // to make image fill the entire body of the website
-  $(".content").css("margin-top",0);
-  $(".content").css("padding-left",0);
-  $(".content").css("padding-right",0);
-  $(".klt-navbar").css("margin-bottom",0);
-
-  // sketchpad
-  var sp = new SketchPad("canvas-test");
-  sp.init(0);
 
 
-    function configNavbar(){
-        $('.content').css('margin-top', kltNavbar.outerHeight() + parseInt(kltNavbar.css('margin-bottom'), 10));
-        $('#navbar-title').text(currentMap.name+": "+currentMap.sceneUnit+" "+(currentSceneIndex+1));
-    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////// QUESTIONS MODAL //////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    $('#feedback').hide();
+    $('#checkA').hide();
+    $('#checkB').hide();
+    $('#checkC').hide();
+    $('#checkD').hide();
+
+    currentQuestion = currentMap.scenes[currentSceneIndex].questions[currentQuestionIndex];
+
+    // sketchpad
+    var sp = new SketchPad("canvas-test");
+    sp.init(0);
 
 
   // console.log(challengesToQuestions.act1[0].title);
@@ -136,47 +113,32 @@ $(document).ready(function() {
     setTimeout(function(){$('#questionModal').modal('toggle'); $('#check'+value).hide();},3000);
   });
 
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////// Helper functions //////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function configNavbar(){
+        $('.content').css('margin-top', kltNavbar.outerHeight() + parseInt(kltNavbar.css('margin-bottom'), 10));
+    }
+
+    function updatePage() {
+        // set nav bar title
+        $('#navbar-title').text(currentMap.name+": "+currentMap.sceneUnit+" "+(currentSceneIndex+1));
+
+        // set background
+        document.getElementById("map-image").innerHTML="<img src='"+currentMap.scenes[currentSceneIndex].image+"' alt='' height='800' width='1400'>";
+    }
+
+
+
+
 });
 
-
-// helper functions
-
-function populateData() {
-
-}
-
-// function setUpDictionaries() {
-// 	// We can use currentAct to index into the list of values and get the proper item
-//     // key (currentMap) -> value (name of scene or image)
-//   mapsToScenes["Medieval"] 		= ["act1", "act2", "act3"];
-// 	mapsToScenes["Space"] 	    = ["voyage1", "voyage2", "voyage3"];
-//   scenesToImages["Medieval"] 	= ["act1.jpg", "act2.png", "act3.png"];
-// 	scenesToImages["Space"]     = ["voyage1.jpg", "voyage2.jpeg", "voyage3.jpg"];
-
-// 	// key (currentMap, currentAct) -> value (list of the names of questions)
-// 	challengesToQuestions["act1"]	 = [compositionOfMatter, lifeOfPi, famousComposers];
-// 	challengesToQuestions["act2"] 	 = [compositionOfMatter, lifeOfPi, famousComposers];
-// 	challengesToQuestions["act3"] 	 = [compositionOfMatter, lifeOfPi, famousComposers];
-// 	challengesToQuestions["voyage1"] = [compositionOfMatter, lifeOfPi, famousComposers];
-// 	challengesToQuestions["voyage2"] = [compositionOfMatter, lifeOfPi, famousComposers];
-// 	challengesToQuestions["voyage3"] = [compositionOfMatter, lifeOfPi, famousComposers];
-// }
-
-
-
-function Question(title, description, choices, correctAnswer) {
-    this.title = title;
-    this.description = description;
-    this.choices = choices;
-    this.correctAnswer = correctAnswer;
-    return this;
-}
-
-var compositionOfMatter = new Question("Composition of Matter", "Which is the first element in the periodic table?", ["Li", "He", "H", "Ne"], "C");
-var lifeOfPi = new Question("Life of Pi", "Rally", [""], "A");
-var famousComposers = new Question("Famous Composers", "Which composer was deaf?", ["Mozart", "Beethoven", "Handel", "Bach"], "B");
 
 $(window).unload(function(){
     console.log("here about to remove chatbox: " + chatName);
     removeChat(chatName);
 });
+
+
