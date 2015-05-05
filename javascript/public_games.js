ebe658a55b9f573f420824e1ab4aefde6998b144
Ami Suzuki <asuzuki@mit.edu>
2015-05-05 14:00:53 -0400

    random games have up to 3 people, not overlapping

--- javascript/public_games.js
+++ javascript/public_games.js
@@ -1,7 +1,8 @@
 // must be called AFTER utilityFuncs.js
 
+var maxNumPlayers = 4;
 var publicGames = [];
-var numGames = 20;
+var numGames = 15;
 var gameTitles = ["we rock", "I <3 space", "Awesomest game ever", "bestestest game", "best 5th graders",
 "the cool game", "we love math group", "Happiest 6th graders", "math masters", "scientists",
 "blueberries", "computer wizards", "chocolate pretzels", "rock stars", "caramel ice cream",
@@ -9,21 +10,34 @@ var gameTitles = ["we rock", "I <3 space", "Awesomest game ever", "bestestest ga
 
 totalUsers = getUserTotal();
 
+var alreadyPlaying = [];
+
 for (var i=0; i<numGames; i++) {
   var map = getRandomMap();
   var gameTitle = gameTitles[ i%gameTitles.length ];
-  var requiredNumPlayers = 4;
+  var actualPlayers = [];
 
-  var game = {
-    title: gameTitle,
-    map: map,
-    players: getRandomUsers(requiredNumPlayers),
-    privateGame: false
+  while (actualPlayers.length === 0 && totalUsers - actualPlayers.length >= 1) { // there are still users that can be placed in a public game
+    var randomPlayers = getRandomUsers(maxNumPlayers-1); // need 1-3 other users;
+    for (var j=0; j<randomPlayers.length; j++) { // check that each player does not overlap with other games' players
+      if (checkIfInObjectArray(randomPlayers[j].name(), alreadyPlaying) === -1) {
+        alreadyPlaying.push(randomPlayers[j]);
+        actualPlayers.push(randomPlayers[j]);
+      }
+    }
   }
 
-  publicGames.push(game);
-  
+  if (actualPlayers.length > 0) {
+    var game = {
+      title: gameTitle,
+      map: map,
+      players: actualPlayers,
+      privateGame: false
+    }
+    publicGames.push(game);
+  }
 }
+
 
 $(document).ready(function() {
   displayPublicGames();
